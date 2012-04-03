e2a15c9e3883753ce4a8c7164672a054543e452e
Dainius Jocas <dainius.jocas@gmail.com>
2012-04-04 00:17:46 +0300

    pataisysim klasifikavimo rezultatus su papildomais parametrais

--- rcode/classification.r
+++ rcode/classification.r
@@ -59,7 +59,7 @@ do_classification <- function(dataset, labels,
   {
     train_data <- t(dataset[best_features[1:i], train_indexes])
     test_data <-  t(dataset[best_features[1:i], test_indexes])
-    model <- svm(x=train_data, y=as.factor(labels[train_indexes]), kernel='linear')#, cost=0.01)
+    model <- best.svm(x=train_data, y=as.factor(labels[train_indexes]), kernel='linear', cost=0.01)
     guess <- predict(model, test_data, decision.values=T)
     print(guess)
     write(c(i, attr(guess, 'decision.values')), file=decision_values_file, ncolumns=1000, append=T)
